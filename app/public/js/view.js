f213d9d636e791c3286e40e51b81382f25b88a3e
Stashun <sashmore288@gmail.com>
2018-01-22 15:23:22 -0500

    api stackoverflow

--- app/public/js/view.js
+++ app/public/js/view.js
@@ -3,37 +3,25 @@
 // URL parameter. Our server then performs the search to grab that character from the Database.
 
 // when user hits the search-btn
-$("#search-btn").on("click", function() {
-  // save the character they typed into the character-search input
-  var searchedCharacter = $("#character-search")
-    .val()
-    .trim();
 
-  // Using a RegEx Pattern to remove spaces from searchedCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  searchedCharacter = searchedCharacter.replace(/\s+/g, "").toLowerCase();
+$(function(){
+  $(".dropdown-menu").on("mouseup", '*', function() {
+    var searchTerm = $(this).attr('data-value');
 
-  // run an AJAX GET-request for our servers api,
-  // including the user's character in the url
-  $.get("/api/" + searchedCharacter, function(data) {
-    // log the data to our console
-    console.log(data);
-    // empty to well-section before adding new content
-    $("#well-section").empty();
-    // if the data is not there, then return an error message
-    if (!data) {
-      $("#well-section").append("<h2> The force is not strong with this one. Your character was not found. </h2>");
-    }
-    else {
-      // otherwise
-      // append the character name
-      $("#well-section").append("<h2>" + data.name + "</h2>");
-      // the role
-      $("#well-section").append("<h3>Role: " + data.role + "</h3>");
-      // the age
-      $("#well-section").append("<h3>Age: " + data.age + "</h3>");
-      // and the force points
-      $("#well-section").append("<h3>Force Points: " + data.forcePoints + "</h3>");
-    }
+    fetch (
+        'https://api.stackexchange.com/2.2/questions?pagesize=5&tagged='+ searchTerm + '&site=stackoverflow'
+    )
+    .then (function (response) {
+        if (response.ok) { return response.json(); }
+    } )
+    .then (function(rJsn) {
+      $('ul.articles').empty();
+      rJsn.items.forEach(function(obj,i) {
+        console.log('EACH OBJECT', obj)
+        $(".articles").append('<li ><a href= '+ obj.link + '>'+ obj.title + '</a></li>')
+      })
+    });
+      console.log($(this).attr('data-value'))
   });
-});
+})
+
