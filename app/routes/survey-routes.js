7b6f0e040a5ba79770683a684833f2f5ddc0a478
Michelle Johnson <bell2936@yahoo.com>
2018-01-23 09:42:07 -0500

    API Post

---
+++ app/routes/survey-routes.js
@@ -0,0 +1,62 @@
+
+// If a user sends data to add a new character...
+  app.post("/api/new", function(req, res) {
+    // Take the request...
+    var glossaryterm = req.body;
+    var db = require("../models");
+
+    module.exports = function(app) {
+
+    // Create a routeName
+
+    // Using a RegEx Pattern to remove spaces from character.name
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+   // var routeName = character.name.replace(/\s+/g, "").toLowerCase();
+
+    // Then add the character to the database using sequelize
+    glossaryterm.create({
+      routeName: routeName,
+      term: glossary.name,
+      lang: glossary.language,
+      definition: glossary.definition,
+      // POST route for saving a new post
+  app.post("/api/GlossaryTerm", function(req, res) {
+    console.log(req.body);
+    db.GlossaryTerm.create({
+      routeName: routeName,
+      term: glossary.name,
+      lang: glossary.language,
+      definition: glossary.definition
+    })
+    .then(function(dbGlossaryTerm) {
+      res.json(dbGlossaryTerm);
+    });
+  });
+
+  // DELETE route for deleting posts
+  app.delete("/api/GlossaryTerm/:id", function(req, res) {
+    db.Post.destroy({
+      where: {
+        id: req.params.GlossaryTerm
+      }
+    })
+    .then(function(dbGlossaryTerm) {
+      res.json(dbGlossaryTerm);
+    });
+  });
+
+  // PUT route for updating posts
+  app.put("/api/GlossaryTerm", function(req, res) {
+    db.Post.update(req.body,
+      {
+        where: {
+          id: req.params.GlossaryTerm
+        }
+      })
+    .then(function(dbGlossaryTerm) {
+      res.json(dbGlossaryTerm);
+    });
+  });
+    });
+  });
+};
